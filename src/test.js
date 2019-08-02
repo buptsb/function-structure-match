b13a2e59ce2e3a014d68550f68f845f7cc541a5e
Cong Zuo <zckevinzc@gmail.com>
2019-08-02 22:17:13 +0800

    Using debug version of `Match` function.

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-import { Match, MatchDebug } from "./index";
+import { Match } from "./index";
 const assert = require("assert");
 
 function test_a(_$de, _$vl) {
@@ -29,9 +29,9 @@ let test_d = function(a, b) {
   return e;
 };
 
-assert.strictEqual(Match(test_a, test_b), true);
-assert.strictEqual(Match(test_a, test_c), false);
-assert.strictEqual(Match(test_a, test_d), true);
+assert.strictEqual(Match(test_a, test_b, true), true);
+assert.strictEqual(Match(test_a, test_c, true), false);
+assert.strictEqual(Match(test_a, test_d, true), true);
 
 let literal_str_a = function() {
   return "1";
@@ -51,9 +51,9 @@ let literal_digit_c = function() {
 let literal_digit_d = function() {
   return b + 2;
 };
-assert.strictEqual(Match(literal_str_a, literal_str_b), true);
-assert.strictEqual(Match(literal_digit_a, literal_digit_b), true);
-assert.strictEqual(Match(literal_digit_c, literal_digit_d), true);
+assert.strictEqual(Match(literal_str_a, literal_str_b, true), true);
+assert.strictEqual(Match(literal_digit_a, literal_digit_b, true), true);
+assert.strictEqual(Match(literal_digit_c, literal_digit_d, true), true);
 
 let this_a = function() {
   this._$rD();
@@ -68,15 +68,28 @@ let this_c = {
     this.a();
   }
 };
-assert.strictEqual(Match(this_a, this_b), true);
-assert.strictEqual(Match(this_a, this_c.fn), true);
+assert.strictEqual(Match(this_a, this_b, true), true);
+assert.strictEqual(Match(this_a, this_c.fn, true), true);
 
 {
   function with_regex_a(_$uX) {
     return _$ni.call(_$uX[_$tp()](), /{\s*return\s*([A-Za-z0-9$_]+);?\s*}/)[1];
   }
   let with_regex_b = function(arg) {
-    return a.call(b[c()](), /abcdeffffffffffffffffffffffffff/)[0];
+    return a.call(b[c()](), /abcdeffffffffffffffffffffff/)[0];
   };
-  assert.strictEqual(Match(with_regex_a, with_regex_b), true);
+  assert.strictEqual(Match(with_regex_a, with_regex_b, true), true);
+}
+
+{
+  // named and unnamed function
+  function _$rs(_$aW) {
+    ++_$ww;
+    _$xZ(3);
+  }
+  let sample = function(_$kE) {
+    ++_$wo;
+    _$xC(3);
+  };
+  assert.strictEqual(Match(_$rs, sample, true), true);
 }
